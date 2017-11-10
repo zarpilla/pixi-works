06f9d16b3f7f6d34000a5a23b3db8f26b372d1ba
Jordi Sabaté <zarpa37@gmail.com>
2017-11-10 13:23:40 +0100

    democracia effect

---
+++ resources/democracia/democracia.js
@@ -0,0 +1,45 @@
+$(function() {
+    console.log( "ready!" );
+
+    var str = 'LLIBERTAT PRESES POLÍTIQUES';
+    for (var i = 0, len = str.length; i < len; i++) {
+        $('#llibertat').append('<span class="visible-false" id="lt-' + i + '">' + str[i] + '</span>');
+    }
+    $('#llibertat').append('<div id="nopararem" class="visible-false nopararem">#nopararem</div>');
+
+    var t = 0;
+    var intervalID = setInterval(function(){
+        //console.log("Interval reached" + t);
+
+        if (t >= 100) {
+            clearInterval(intervalID);
+            doEnd();
+            
+        }
+
+        for (var i = 0, len = str.length; i < len; i++) {            
+            var p = Math.floor(Math.random() * 100); // 0-99
+            var el = $('#lt-' + i);            
+            if (p <= t) {
+                el.removeClass('visible-false');
+            }
+            else if (!el.hasClass('visible-false')) {
+                el.addClass('visible-false');
+            }            
+        }
+        t = t + 5;
+    }, 250);
+
+    function doEnd() {
+
+        $( "#body" ).animate({
+            backgroundColor: '#ffffff'
+          }, 2000, function() {
+            // Animation complete.
+            $('#nopararem').removeClass('visible-false');
+            $('#llibertat').addClass('llibertat-border');
+          });
+
+    }
+    
+});
